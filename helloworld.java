ccc3a57b29cc697937ebe600b55d2e290c73346d
sankaramoorthy10 <sankaramoorthy10@gmail.com>
2024-07-19 19:49:25 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
      System.out.println("HELLO PETER ");
-      System.out.println("Hello Batch batch35 WITH FOR STUDENTS RUNNING in front of hari  and vinod and abc iman in making CI automated vikas teach devops CI proecss  ");
     }  
 } 
